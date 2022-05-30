82227556e24f68220cf7e0dda7f22e66536bd846
IllyaKolisnichenko <kolisnichenko.ilya@yandex.ru>
2022-05-30 22:02:09 +0300

    add mail file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int val1 = 10;
+    
+    do{
+        cout << val1 << endl;
+        val1++;
+    }while( true );
+    
+    return 0;
+}
