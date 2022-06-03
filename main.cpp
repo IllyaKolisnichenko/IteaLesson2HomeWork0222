18be4231f1c5abd44f013376ee0b1fb9d3c3cb73
slavamishch1979@gmail.com <slavamishch1979@gmail.com>
2022-06-03 05:08:47 +0300

    homeWork

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <cstdlib>
 
 int NumberDrops[100]; //an array in which we write 100 drops of a dice
-int MassNumbersDrops[5]; //an array in which we write the number of rolls of each side of the dice
 int NumberOne = 0; //number of rolls 1
 int NumberTwo = 0;//number of rolls 2
 int NumberThree = 0;//number of rolls 3
@@ -13,32 +12,20 @@ int MaxNumbersDrops = 0; //variable in which we write max number of rolls
 int MinNumbersDrops = 0; // variable in which we write max number of rolls
 int NumberMaxDrops = 0; // the number wich droped max numbers times число 
 int NumberMinDrops = 0; //the number wich droped min numbers times число 
-int SizeMass = 0;
-int Max = 0, Min = 0;
-
 
 int getNumber(int min, int count)
 {
     return min + rand() % count;
 }
 
-
-
 int main()
 {
     srand(time(nullptr));
 
+      //---------- find the number of rolls on each side------------------------
     for (int i = 0; i < 100; i++)
     {
-        NumberDrops[i] = getNumber(1, 6); // fill the array a randon numbers from 1 to 6
-    }
-
-
-    //---------- find the number of rolls on each side------------------------
-    for (int i = 0; i < 100; i++)
-    {
-
-        switch (NumberDrops[i])
+        switch (NumberDrops[i] = getNumber(1, 6);)
         {
         case 1:
             NumberOne++;
@@ -58,21 +45,15 @@ int main()
         case 6:
             NumberSix++;
             break;
-
-
         }
 
     }
-    //-------------------------------------------------------------------------------------------------
-
-
+ 
     for (int i = 0; i < 100; i++)
     {
         std::cout << NumberDrops[i];
-
     }
     std::cout << "\r\n";
-    std::cout << NumberDrops << "\n\r";
     std::cout << "Numbers of drops 1 - " << NumberOne << "\n\r";
     std::cout << "Numbers of drops 2 - " << NumberTwo << "\n\r";
     std::cout << "Numbers of drops 3 - " << NumberThree << "\n\r";
@@ -80,18 +61,10 @@ int main()
     std::cout << "Numbers of drops 5 - " << NumberFive << "\n\r";
     std::cout << "Numbers of drops 6 - " << NumberSix << "\n\r";
 
-    MassNumbersDrops[0] = NumberOne;
-    MassNumbersDrops[1] = NumberTwo;
-    MassNumbersDrops[2] = NumberThree;
-    MassNumbersDrops[3] = NumberFour;
-    MassNumbersDrops[4] = NumberFive;
-    MassNumbersDrops[5] = NumberSix;
-
-
-    Max = MassNumbersDrops[0];
-    Min = MassNumbersDrops[0];
-
-    SizeMass = sizeof(MassNumbersDrops) / sizeof(int);
+    int MassNumbersDrops[5] = { NumberOne; NumberTwo, NumberThree, NumberFour, NumberFive,  NumberSix };
+    int Max = MassNumbersDrops[0];
+    int Min = MassNumbersDrops[0];
+    int SizeMass = sizeof(MassNumbersDrops) / sizeof(int);
 
     for (int i = 0; i <= SizeMass; i++)
     {
@@ -99,19 +72,15 @@ int main()
         {
             Max = MassNumbersDrops[i];
             NumberMaxDrops = i;
-
-
         }
-    }
 
-    for (int i = 0; i <= SizeMass; i++)
-    {
         if (MassNumbersDrops[i] < Min)
         {
             Min = MassNumbersDrops[i];
             NumberMinDrops = i;
         }
     }
+
     NumberMaxDrops = NumberMaxDrops + 1;
     NumberMinDrops = NumberMinDrops + 1;
     std::cout << "\r\n";
@@ -119,11 +88,7 @@ int main()
     std::cout << "The number what droped min times - " << NumberMinDrops << "- Numbers times - " << Min;
 
 
-    // For get rand value, use getNumber
-    // example ( min = 100, count = 150 ) - int randValue{ getNumber( 100, 150 ) };
-
-    // Add your code here
-    return 0;
+return 0;
 }
 
 
