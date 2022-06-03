bc4400ec5aa992d800ee14d6c4e895f5de083a1f
slavamishch1979@gmail.com <slavamishch1979@gmail.com>
2022-06-03 05:31:07 +0300

    homeWork

--- main.cpp
+++ main.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 #include <cstdlib>
 
-int NumberDrops[100]; //an array in which we write 100 drops of a dice
-int NumberOne = 0; //number of rolls 1
-int NumberTwo = 0;//number of rolls 2
-int NumberThree = 0;//number of rolls 3
-int NumberFour = 0;//number of rolls 4
-int NumberFive = 0;//number of rolls 5
-int NumberSix = 0;//number of rolls 6
-int MaxNumbersDrops = 0; //variable in which we write max number of rolls
-int MinNumbersDrops = 0; // variable in which we write max number of rolls
-int NumberMaxDrops = 0; // the number wich droped max numbers times число 
-int NumberMinDrops = 0; //the number wich droped min numbers times число 
-
 int getNumber(int min, int count)
 {
     return min + rand() % count;
@@ -25,25 +13,25 @@ int main()
       //---------- find the number of rolls on each side------------------------
     for (int i = 0; i < 100; i++)
     {
-        switch (NumberDrops[i] = getNumber(1, 6);)
+        switch (int NumberDrops[i] = getNumber(1, 6);)
         {
         case 1:
-            NumberOne++;
+           int NumberOne++;
             break;
         case 2:
-            NumberTwo++;
+           int NumberTwo++;
             break;
         case 3:
-            NumberThree++;
+           int NumberThree++;
             break;
         case 4:
-            NumberFour++;
+           int NumberFour++;
             break;
         case 5:
-            NumberFive++;
+           int NumberFive++;
             break;
         case 6:
-            NumberSix++;
+           int NumberSix++;
             break;
         }
 
@@ -62,30 +50,26 @@ int main()
     std::cout << "Numbers of drops 6 - " << NumberSix << "\n\r";
 
     int MassNumbersDrops[5] = { NumberOne; NumberTwo, NumberThree, NumberFour, NumberFive,  NumberSix };
-    int Max = MassNumbersDrops[0];
-    int Min = MassNumbersDrops[0];
     int SizeMass = sizeof(MassNumbersDrops) / sizeof(int);
 
     for (int i = 0; i <= SizeMass; i++)
     {
         if (MassNumbersDrops[i] > Max)
         {
-            Max = MassNumbersDrops[i];
-            NumberMaxDrops = i;
+           int  Max = MassNumbersDrops[i];
+           int  NumberMaxDrops = i;
         }
 
         if (MassNumbersDrops[i] < Min)
         {
-            Min = MassNumbersDrops[i];
-            NumberMinDrops = i;
+           int Min = MassNumbersDrops[i];
+           int NumberMinDrops = i;
         }
     }
 
-    NumberMaxDrops = NumberMaxDrops + 1;
-    NumberMinDrops = NumberMinDrops + 1;
     std::cout << "\r\n";
-    std::cout << "The number what droped max times - " << NumberMaxDrops << " - Numbers times - " << Max << "\r\n";
-    std::cout << "The number what droped min times - " << NumberMinDrops << "- Numbers times - " << Min;
+    std::cout << "The number what droped max times - " << NumberMaxDrops+1 << " - Numbers times - " << Max << "\r\n";
+    std::cout << "The number what droped min times - " << NumberMinDrops+1 << "- Numbers times - " << Min;
 
 
 return 0;
