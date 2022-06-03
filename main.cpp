9af9cfd449bbd10913f6ec7687a7a08053b5d1d3
ZaloginYuriy <yuriyzalogin31031981@gmail.com>
2022-06-04 01:09:03 +0300

    kub

--- main.cpp
+++ main.cpp
@@ -1,19 +1,51 @@
-#include <iostream>
-#include <cstdlib>
-
-int getNumber( int min, int count )
-{
-    return min + rand() % count ;
-}
-
+#include <iostream>  
+#include<Windows.h>
+#include<math.h>
+//using namespace std;
 int main()
 {
-    srand( time( nullptr ) );
+	setlocale(LC_ALL, "");
+	srand(time(0));
+	int faster = 0, fast = 0, sum1 = 0, sum2 = 0, same = 0;
+	do
+	{
+		for (int i = 0;i < 100;i++)
+		{
+			if (i % 2 == 0)
+			{
+				faster = rand() % 6 + 1;
+				fast = rand() % 6 + 1;
+				sum1 += faster + fast;
+				std::cout << faster << "/" << fast << "\n";
+				Sleep(10);
+				system("Cls");
+			}
+			else
+			{
+				faster = rand() % 6 + 1;
+				fast = rand() % 6 + 1;
+				sum2 += faster + fast;
+				std::cout << faster << "/" << fast << "\n";
+				Sleep(10);
+				system("Cls");
+			}
+		}
+		std::cout << "сумма бросков первого игрока:" << sum1 << "\n";
+		std::cout << "сумма бросков второго игрока:" << sum2 << "\n";
 
-    // For get rand value, use getNumber
-    // example ( min = 100, count = 150 ) - int randValue{ getNumber( 100, 150 ) };
+		if (sum1 > sum2)
+		{
+			same = sum1 - sum2;
+			std::cout << "результат бросков первого игрока юольше на" << same << "\n";
+		}
+		else if (sum2 > sum1)
+		{
+			same = sum2 - sum1;
+			std::cout << "результат бросков второго игрока юольше на" << same << "\n";
 
-    // Add your code here
-    return 0;
+		}
+
+		break;
+	} while (true);
 }
 
