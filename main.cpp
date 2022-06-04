acc7da0759fb0a777843ec6263e8baa9fda3cd4b
unknown <nimas.andriy@gmail.comgit config --global user.email nimas.andriy@gmail.comgit config --global user.email nimas.andriy@gmail.com>
2022-06-04 21:29:32 +0300

    homework2_1

--- main.cpp
+++ main.cpp
@@ -4,65 +4,56 @@ using namespace std;
 
 int main()
 {
-    srand(time(nullptr));
-    int RandNumber, Number1 {}, Number2 {}, Number3 {}, Number4 {}, Number5 {}, Number6 {} ;
-    int max_index {0}, min_index {0};
-    int Maximum {0}, Minimum {0};
+    srand( time ( nullptr ));
+    const int numberThrows {100};
+    int randNumber, number1 {}, number2 {}, number3 {}, number4 {}, number5 {}, number6 {} ;
+    int maxIndex {}, minIndex {};
     
-    for (int i=0; i<100; i++)
+    for (int i{}; i < 100; i++)
     {
-        RandNumber = 1 + rand() % 6;
-        switch (RandNumber)
+        randNumber = 1 + rand() % 6; //generate random nubber from 1 to 6
+        switch (randNumber)
         {
             case 1:
-                Number1++;
+                number1++;
                 break;
             case 2:
-                Number2++;
+                number2++;
                 break;
             case 3:
-                Number3++;
+                number3++;
                 break;
             case 4:
-                Number4++;
+                number4++;
                 break;
             case 5:
-                Number5++;
+                number5++;
                 break;
             case 6:
-                Number6++;
+                number6++;
                 break;
             }
     }
 
-    cout << "Number 1 = " << Number1 << "\n";
-    cout << "Number 2 = " << Number2 << "\n";
-    cout << "Number 3 = " << Number3 << "\n";
-    cout << "Number 4 = " << Number4 << "\n";
-    cout << "Number 5 = " << Number5 << "\n";
-    cout << "Number 6 = " << Number6 << "\n";
+    cout << "Number 1 = " << number1 << "\n";
+    cout << "Number 2 = " << number2 << "\n";
+    cout << "Number 3 = " << number3 << "\n";
+    cout << "Number 4 = " << number4 << "\n";
+    cout << "Number 5 = " << number5 << "\n";
+    cout << "Number 6 = " << number6 << "\n";
     
     //Looking for minumum and maximum
+    const int arrSize { 6 };
+    int arrayNumber[ arrSize ] {number1, number2, number3, number4, number5, number6};
     
-    int Array[6] {Number1, Number2, Number3, Number4, Number5, Number6};
-    Maximum = Array[0];
-    Minimum = Array[0];
-    
-    
-    for (int i = 0; i < 6; ++i)
+    for ( int i{}; i < arrSize; ++i )
     {
-       if (Array[min_index] > Array[i])
-       {
-            min_index = i;
-       }
-       if (Array[max_index] < Array[i])
-       {
-            max_index = i;
-       }
-       
+       if ( arrayNumber[minIndex] > arrayNumber[i] ) minIndex = i;
+       if ( arrayNumber[maxIndex] < arrayNumber[i] ) maxIndex = i;  
     }
-    cout << "Maximum = " << ++max_index << "\n";
-    cout << "Minimum = " << ++min_index << "\n";
+    
+    cout << "Maximum = " << ++maxIndex << "\n";
+    cout << "Minimum = " << ++minIndex << "\n";
     
     return 0;
 }
