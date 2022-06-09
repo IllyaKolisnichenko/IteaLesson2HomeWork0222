d0a4f1a85c3a1c4f9c331e18f525060db71e208a
vania <vanyya1254@gmail.com>
2022-06-09 15:41:39 +0300

    test

--- main.cpp
+++ main.cpp
@@ -1,19 +1,105 @@
 #include <iostream>
 #include <cstdlib>
+using namespace std;
 
-int getNumber( int min, int count )
+int NumberDrops[100];
+int MassNumberDrops[5];
+int NumberOne = 0;
+int NumberTwo = 0;
+int NumberThree = 0;
+int NumberFour = 0;
+int NumberFive = 0;
+int NumberSix = 0;
+int SizeMass = 0;
+int Max = 0, Min = 0;
+
+int getNumber(int min, int count)
 {
-    return min + rand() % count ;
+	return min + rand() % count;
+	cout << min;
 }
 
 int main()
 {
-    srand( time( nullptr ) );
+	setlocale(LC_ALL, "ukr");
+	srand(time(nullptr));
+	for (int i = 0; i < 100; i++)
+	{
+		NumberDrops[i] = getNumber(1, 6);
+	}
+	for (int i = 0; i < 100; i++)
+	{
+		switch (NumberDrops[i])
+		{
+		case 1:
+			NumberOne++;
+			break;
+		case 2:
+			NumberTwo++;
+			break;
+		case 3:
+			NumberThree++;
+			break;
+		case 4:
+			NumberFour++;
+			break;
+		case 5:
+			NumberFive++;
+			break;
+		case 6:
+			NumberSix++;
+			break;
+		}
+	}
+	for (int i = 0; i < 100; i++)
+	{
+		cout << "Кiлькiсть випадання сторони 1 - " << NumberOne << endl;
+		cout << "Кiлькiсть випадання сторони 2 - " << NumberTwo << endl;
+		cout << "Кiлькiсть випадання сторони 3 - " << NumberThree << endl;
+		cout << "Кiлькiсть випадання сторони 4 - " << NumberFour << endl;
+		cout << "Кiлькiсть випадання сторони 5 - " << NumberFive << endl;
+		cout << "Кiлькiсть випадання сторони 6 - " << NumberSix << endl;
 
-    // For get rand value, use getNumber
-    // example ( min = 100, count = 150 ) - int randValue{ getNumber( 100, 150 ) };
+		MassNumberDrops[0] = NumberOne;
+		MassNumberDrops[1] = NumberTwo;
+		MassNumberDrops[2] = NumberThree;
+		MassNumberDrops[3] = NumberFour;
+		MassNumberDrops[4] = NumberFive;
+		MassNumberDrops[5] = NumberSix;
 
-    // Add your code here
-    return 0;
+		Max = MassNumberDrops[0];
+		Min = MassNumberDrops[0];
+		SizeMass = sizeof(MassNumberDrops) / sizeof(int);
+		int NumberMaxDrops = 0;
+		for (int i = 0; i <= SizeMass; i++)
+		{
+
+			if (MassNumberDrops[i] > Max)
+			{
+				Max = MassNumberDrops[i];
+				NumberMaxDrops = i;
+
+			}
+		}
+		int NumberMinDrops = 0;
+		for (int i = 0; i <= SizeMass; i++)
+		{
+
+			if (MassNumberDrops[i] < Min)
+			{
+				Min = MassNumberDrops[i];
+				NumberMinDrops = i;
+			}
+		}
+		NumberMaxDrops = NumberMaxDrops + 1;
+		NumberMinDrops = NumberMinDrops + 1;
+		cout << "\r\n";
+		cout << "Максимальна кiлькiсть випадань - сторона " << NumberMaxDrops << ": кiлькiсть випадань " << Max << endl;
+		cout << "Мiнiмальна кiлькiсть випадань - сторона " << NumberMinDrops << ": кiлькiсть випадань " << Min << endl;
+
+
+		return 0;
+
+	}
 }
 
